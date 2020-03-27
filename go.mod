680d3e2161fc349f43c8191bb7ebc9a8e653d8f5
Dustin Lange <dustin.lange94@gmail.com>
2020-03-27 23:23:45 +0100

    add post testing and gson

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dustinla/router
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/tidwall/gjson v1.6.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	. "github.com/dustinla/router/server"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+type url struct {
+	Url string `json:"url"`
+}
+
 func main() {
-	StartServer("1234")
+	//StartServer("1234")
+
+	test := url{Url: "http://polyglot.ninja/golang-making-http-requests/"}
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(test)
+	resp, err := http.Post("https://rel.ink/api/links/", "application/json", b)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	str3 := bytes.NewBuffer(bodyBytes).String()
+	res := gjson.Get(str3, "url")
+
+	fmt.Println(res)
 }
